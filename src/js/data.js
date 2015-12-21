20a15eeb746f4e74c8c12f8cc2f251dee7ee839f
Joshua@PowerMac <jdorenkamp@powercreative.com>
2015-12-21 13:42:37 -0500

    adding a status to candidates

--- src/js/data.js
+++ src/js/data.js
@@ -5,86 +5,103 @@ var data = {
       url: "https://jeb2016.com/?lang=en",
       img: "http://takeastand.aarp.org/wp-content/uploads/Jeb-Bush-e1443548600829.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "Ben Carson",
       url: "https://www.bencarson.com/",
       img: "http://www.impact2016.org/wp-content/uploads/2015/09/16475754279_f36d9616ec_k-350x350.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "Chris Christie",
       url: "https://www.chrischristie.com/",
       img: "http://cdn.fwd.us/media/legislators/avatars/000/000/922/medium-1435233424.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "Hillary Clinton",
       url: "https://www.hillaryclinton.com/",
       img: "http://cdn.fwd.us/media/legislators/avatars/000/000/927/medium-1438710287.jpg",
       class: 'dem',
+      active: true
     }, {
       name: "Ted Cruz",
       url: "https://www.tedcruz.org/",
       img: "http://cdn.fwd.us/media/legislators/avatars/000/000/185/medium-1438710925.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "Carly Fiorina",
       url: "https://www.carlyforpresident.com/",
       img: "http://dentongop.org/wp-content/uploads/2015/07/carly-fiorina-headshot-350x350.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "Jim Gilmore",
       url: "http://www.gilmoreforamerica.com/",
       img: "http://cdn.fwd.us/media/legislators/avatars/000/000/932/medium-1436386791.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "Lindsey Graham",
       url: "http://www.lindseygraham.com/",
       img: "http://dentongop.org/wp-content/uploads/2015/07/Lindsey-graham-headshot1-350x350.jpg",
       class: 'gop',
+      active: false
     }, {
       name: "Mike Huckabee",
       url: "http://www.mikehuckabee.com/",
       img: "http://cdn.fwd.us/media/legislators/avatars/000/000/919/medium-1438709332.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "John Kasich",
       url: "https://johnkasich.com/",
       img: "http://cdn.fwd.us/media/legislators/avatars/000/000/928/medium-1435233439.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "Martin O'Malley",
       url: "https://martinomalley.com/",
       img: "http://img1.rnkr-static.com/user_node_img/77/1532673/C350/martin-o-malley-politicians-photo-u5.jpg",
       class: 'dem',
+      active: true
     }, {
       name: "George Pataki",
       url: "http://www.georgepataki.com/",
       img: "http://cdn.fwd.us/media/legislators/avatars/000/000/929/medium-1438709505.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "Rand Paul",
       url: "https://www.randpaul.com/",
       img: "http://cdn.fwd.us/media/legislators/avatars/000/000/615/medium-1438709464.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "Marco Rubio",
       url: "https://marcorubio.com/the-real-difference-between-marco-rubio-and-ted-cruz/",
       img: "http://www.ooyuz.com/images/2015/8/15/1442292498863.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "Bernie Sanders",
       url: "https://secure.berniesanders.com/",
       img: "http://img1.rnkr-static.com/user_node_img/29/572212/C350/bernie-sanders-politicians-photo-u4.jpg",
       class: 'dem',
+      active: true
     }, {
       name: "Rick Santorum",
       url: "http://www.ricksantorum.com/",
       img: "http://cdn.fwd.us/media/legislators/avatars/000/000/923/medium-1438709921.jpg",
       class: 'gop',
+      active: true
     }, {
       name: "Donald Trump",
       url: "https://www.donaldjtrump.com/",
       img: "http://www.impact2016.org/wp-content/uploads/2015/09/trump-head1-350x350.jpg",
       class: 'gop',
+      active: true
     }
   ],
   "gop": [{
