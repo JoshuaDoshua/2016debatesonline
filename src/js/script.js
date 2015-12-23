1abaa2f4a5db3c819ef475036ca2d639b47430f5
JoshDell <joshua@dkampesign.com>
2015-12-22 23:22:34 -0500

    js keep track of hash

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function($) {
 
   var $body = $('body');
+  var current = "#";
 
   var mainLinks = $('.site-nav a').on('click', function(e) {
     e.preventDefault();
@@ -8,20 +9,41 @@ jQuery(document).ready(function($) {
       .addClass('open')
       .removeClass('dem gop')
       .addClass(this.hash.replace('#',''));
+    current = this.hash;
+    console.log(current);
   });
+
+  var getCurrentSection = function() {
+    if ($body.hasClass('candidates')) {
+      return "#candidates";
+    } else if ($body.hasClass('dem')) {
+      return "#dem";
+    } else if ($body.hasClass('gop')) {
+      return "#gop";
+    }
+  };
+
+  var goToSection = function() {
+    console.log('load section on pageload via loation.hash');
+  };
 
   var backLink = $('#back').on('click', function(e) {
     e.preventDefault();
     if ($body.hasClass('candidates')) {
       $body.removeClass('candidates');
+      current = getCurrentSection();
     } else {
       $body.removeClass('open dem gop');
+      current = "/";
     }
+    console.log(current);
   });
 
   var showCandidates = $('#show-candidates').on('click', function(e) {
     e.preventDefault();
     $body.addClass('candidates');
+    current = "#candidates";
+    console.log(current);
   });
 
   var modalLinks = $('.modal-link').magnificPopup({
@@ -29,6 +51,16 @@ jQuery(document).ready(function($) {
     mainClass: 'mfp-fade',
     removalDelay: 160,
     preloader: false,
+    callbacks: {
+      open: function() {
+        current = this.currItem.src;
+        console.log(current);
+      },
+      close: function() {
+        current = getCurrentSection();
+        console.log(current);
+      }
+    }
   });
 
   var videoLinks = $('.videos-link.video').magnificPopup({
@@ -36,6 +68,16 @@ jQuery(document).ready(function($) {
 		mainClass: 'mfp-fade',
 		removalDelay: 160,
 		preloader: false,
+    callbacks: {
+      open: function() {
+        current = "#" + this.ev[0].dataset.label;
+        console.log(current);
+      },
+      close: function() {
+        current = getCurrentSection();
+        console.log(current);
+      }
+    }
   });
 
 });
